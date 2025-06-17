4832efd8250d46fccbfa52e4d3de5cbbc209f5ab
onatustun <o@ust.sh>
2025-06-17 19:53:16 +0100

    flake.nix alejandra, helix lsp fix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,31 @@
 {
+  description = "nodejs typescript environment";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit
-    inputs;
-  } {
-    systems = import inputs.systems;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
 
-    perSystem = {
-      pkgs,
-      ...
-    }: {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nil
-          nodejs
-          nodePackages.prettier
-          tailwindcss
-          tailwindcss-language-server
-          typescript-language-server
-          vscode-langservers-extracted
-        ];
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            alejandra
+            nil
+            nodejs
+            nodePackages.prettier
+            tailwindcss
+            tailwindcss-language-server
+            typescript-language-server
+            vscode-langservers-extracted
+          ];
+        };
       };
     };
-  };
 }
